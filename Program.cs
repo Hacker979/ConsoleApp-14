36beb8c1f8556a06b52bf8deeb6d398e5c52ff53
Kab-35-16 <Kab-35-16@KAB-31-11->
2024-12-17 08:50:14 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_14
+{
+    class Program
+    {
+        static void Func()
+        {
+            int a = 1;
+            while (a <= 10)
+            {
+                Console.WriteLine(a);
+                a++;
+            }
+        }
+        /// <summary>
+        /// Задание 1. Вывести на экран в столбик первые 10 натуральных чисел (циклы с условием, цикл с параметром)
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadLine();
+        }
+    }
+}
