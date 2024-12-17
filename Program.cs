0332f4ca1798d2ae03bce870d54ba8d629af043b
Kab-35-16 <Kab-35-16@KAB-31-11->
2024-12-17 09:48:34 +0200

    vbn

--- Program.cs
+++ Program.cs
@@ -10,17 +10,41 @@ namespace ConsoleApp_14
     {
         static void Func()
         {
-            for(int a = 1; a<=5; a++)
+            for (int x = 1; x <= 5; x++)
             {
-                Console.WriteLine("Привет!");
+                for(int b = 5; b > x; b--)
+                {
+                    Console.Write(" ");
+                }
+                for (int a = 1; a <= 10; a++)
+                {
+                    Console.Write("#");
+                }
+                Console.Write("\n");
+                
             }
+            
         }
         /// <summary>
-        /// Задание 3. Вывести в столбик 5 раз слово «Привет!» (циклы с условием)
+        /// Задание 4. С использованием цветового оформления консоли:
+        /// а) вывести на экран горизонтальную строку из 18 символов;
+        /// б) вывести на экран вертикальную строку из 25 символов.
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            if (Console.CapsLock == true)
+            { Console.WriteLine("The Caps Lock key is ON."); }
+            else
+            { Console.WriteLine("The Caps Lock key is OFF."); }
+            if (Console.NumberLock == true)
+            { Console.WriteLine("The Num Lock key is ON."); }
+            else
+            { Console.WriteLine("The Num Lock key is OFF."); }
+
+            Console.Title = "Работа с консолью";
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Gray;
             Func();
             Console.ReadLine();
         }
